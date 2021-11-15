6149b7906c48c621627da4a9e4a302c0b53835a9
ghassenroudesly <roudeslyroudesly@gmail.com>
2021-11-15 17:17:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="images" style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Shoes Shop</h1>
+        <img src="./img1.jpg" alt="phones" />
+        <img width="560" height="315" src="./img2.jpg" alt="phones" />
+      </div>
+    
+      <   iframe width="560" height="315" src='https://www.youtube.com/embed/lNvVqcfPYMY'
+        frameborder='15'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+/>
+    </div>
+  );
+}
+
+export default App;
+
